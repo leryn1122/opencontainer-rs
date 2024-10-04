e21d74a68dd336857d0494581963715283cbc042
leryn <pinktin@sina.com>
2024-10-04 12:33:53 +0800

    fix: hotfix libcri proto build

--- crates/libcri/build.rs
+++ crates/libcri/build.rs
@@ -1,16 +1,19 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let mut config = prost_build::Config::new();
+  config.btree_map(&["."]);
+
   tonic_build::configure()
     .build_server(true)
     .build_client(true)
     .build_transport(true)
-    .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+    // .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
     .compile_protos(&["resources/proto/v1.proto"], &["resources/proto/"])?;
 
   tonic_build::configure()
     .build_server(true)
     .build_client(true)
     .build_transport(true)
-    .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+    // .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
     .compile_protos(&["resources/proto/v1alpha2.proto"], &["resources/proto/"])?;
 
   Ok(())
